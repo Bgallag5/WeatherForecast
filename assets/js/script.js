0bbbb112246148fe83d99733273c041625be526e
bengallagher <bgallag5@gmail.com>
2021-07-09 08:37:00 -0400

    oneday and fiveday both populate correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,82 @@
-//---------------------------Notes----------------------//-------------------Notes--------------------------------------//
-//
-// EXAMPLE CALLS:   api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e09ebb7db49ee70223da23fbbc92a143
-//                 https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-//                 https://api.openweathermap.org/data/2.5/weather?q={cityname},{statecode},{countrycode}&appid={APIkey}
-
-
-//----------------------Notes---------------------------//------------------------------Notes---------------------------//
 
 const apiKey = "e09ebb7db49ee70223da23fbbc92a143"
 const todaysWeather = {};
 
-function getOneDay(){
+function getOneDay(city){
     var place = document.querySelector("#citySearch").value;
+    console.log(city)
+    // var pastCity = 
     // var location = place.textContent
-    console.log(place)
 //call API based on loaction
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${apiKey}`)
 .then(response => response.json())
 .then(function(response){
-    console.log(response)
+    console.log(response);
+    var location = response.name;
     var temp = response.main.temp;
     var latitude = response.coord.lat;
     var longitude = response.coord.lon;
-    var datetime = response.dt * 1000; //convert Unix UTC to a readable date and time
-    var dtMili = new Date(datetime)
-    var humanDate = dtMili.toLocaleString();
+    getFiveDay(latitude, longitude, location);
+})   
+}
 
- 
+function getFiveDay(latitude, longitude, location){
 fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}`)
 .then(response => response.json())
 .then(function(data){
     console.log(data)
-    todaysWeather.temp= ((temp - 273.15) * 1.8) + 32 
-    todaysWeather.humidity = response.main.humidity; 
-    todaysWeather.wind = response.wind.speed + "MPH";
-    todaysWeather.date = humanDate;
+    var current = data.current
+    var city = location;
+    todaysWeather.temp= Math.round(((current.temp - 273.15) * 1.8) + 32) 
+    todaysWeather.humidity = current.humidity; 
+    todaysWeather.wind = current.wind_speed + "MPH";
     todaysWeather.uvIndex= data.current.uvi
+    var datetime = data.current.dt * 1000; //convert Unix UTC to a readable date and time
+    var dtMili = new Date(datetime)
+    var humanDate = dtMili.toLocaleDateString();
+    todaysWeather.date = humanDate;
     console.log(todaysWeather);
+    document.getElementById('oneDay').innerHTML="";
+    document.querySelector('.fiveDayWeather').innerHTML="";
+
+    $('#oneDay').append(
+        `<div class= "card">
+        <h3>${city}, ${todaysWeather.date}</h3>
+        <p> Temp: ${todaysWeather.temp}</p>
+        <p> Wind Speed: ${todaysWeather.wind}</p>
+        <p> Humidity: ${todaysWeather.humidity}</p>
+        </div> `
+    );
  
-})
-})
+    for(i=1; i < 6; i++){
+        var list = data.daily[i]
+        var temps = list.temp.day;
+        var fahenehit = Math.round(((temps - 273.15) * 1.8) + 32) 
+        var wind = list.wind_speed;
+        var uv = list.uvi;
+        var humid = list.humidity;
+        var dt = list.dt * 1000
+        var date = new Date(dt).toLocaleDateString();
 
+        $('.fiveDayWeather').append(
+            `<div class = "card" style="width: 12rem">
+            <h5> Date: ${date}</h5>
+            <p> Temp: ${fahenehit}</p>
+            <p> Wind Speed: ${wind}</p>
+            <p> Humidity: ${humid}</p>
+            </div>`
+        )
+    }
+})
 }
 
-function getFiveDay(){
-    //call API via fiveday call
-    //grab relevent data (use JQuery)
-}
+$('.btn').on("click", function(){
+    
+    var city = $('#citySearch').val().trim()
+    console.log(city)
+    var newButton = document.createElement("button")
+  newButton.innerHTML = city;
+  newButton.setAttribute("onclick", function(city){
+    
+  })
+})
